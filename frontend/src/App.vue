a6763da8fe5d8477e6a2381b939c4a2ce2ed88d1
Ryan Hubbuck <r.hubbuck@tbp.org>
2025-09-29 12:56:26 -0400

    clean up styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,19 +19,3 @@ const authStore = useAuthStore()
     </div>
   </div>
 </template>
-
-<style>
-.app-wrapper {
-  width: 100%;
-  min-height: 100vh;
-}
-
-.main-content {
-  min-height: 100vh;
-  width: 100%;
-}
-
-.main-content.with-navbar {
-  min-height: calc(100vh - 56px);
-}
-</style>
