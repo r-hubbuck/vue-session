1425eadf254ac6f1a379296fccb0510ca4e19739
Ryan Hubbuck <r.hubbuck@tbp.org>
2025-10-13 11:12:28 -0400

    update settings for mysql

--- core/settings.py
+++ core/settings.py
@@ -94,11 +94,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME', 'your_db_name'),
-            'USER': os.getenv('DB_USER', 'your_db_user'),
-            'PASSWORD': os.getenv('DB_PASSWORD', ''),
-            'HOST': os.getenv('DB_HOST', 'localhost'),
-            'PORT': os.getenv('DB_PORT', '3306'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('localhost'),
+            'PORT': os.getenv('3306'),
         }
     }
 
